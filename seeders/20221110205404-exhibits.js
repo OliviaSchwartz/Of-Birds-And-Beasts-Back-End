b481905ce88338608800ce0039e1de4eb7c188f7
David Caldwell <doubledc92@gmail.com>
2022-11-10 16:20:55 -0500

    added exhitbits to seerder file

--- seeders/20221110205404-exhibits.js
+++ seeders/20221110205404-exhibits.js
@@ -1,25 +1,45 @@
-'use strict';
+'use strict'
+
+const { query } = require('express')
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+  async up(queryInterface, Sequelize) {
+    return queryInterface.bulkInsert('exhibits', [
+      {
+        name: 'Aquarium',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        name: 'Aviary',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        name: 'Forest',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        name: 'Desert',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        name: 'Reptiles',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        name: 'Frozen Tundra',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }
+    ])
   },
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+  async down(queryInterface, Sequelize) {
+    return queryInterface.bulkDelete('exhibits', null, {})
   }
-};
+}
