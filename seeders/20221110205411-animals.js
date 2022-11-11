d52ede029380ec043cfa3471abc5ac40cfc54a96
David Caldwell <doubledc92@gmail.com>
2022-11-11 13:09:20 -0500

    not showing full animals on pull

--- seeders/20221110205411-animals.js
+++ seeders/20221110205411-animals.js
@@ -5,7 +5,6 @@ module.exports = {
   async up(queryInterface, Sequelize) {
     return queryInterface.bulkInsert('animals', [
       {
-
         name: 'Owl',
         species: 'Tyto Alba',
         information:
@@ -92,7 +91,8 @@ module.exports = {
           'The chicken is a domesticated junglefowl species, with attributes of wild species such as the grey and the Ceylon junglefowl that are originally from Southeastern Asia. Rooster or cock is a term for an adult male bird, and a younger male may be called a cockerel. A male that has been castrated is a capon.',
         image: './chickens.jpg',
         exhibit_Id: 2
-
+      },
+      {
         name: 'Whale Shark',
         species: 'Rhincodon typus',
         information:
@@ -203,13 +203,11 @@ module.exports = {
           'Spider monkeys are New World monkeys belonging to the genus Ateles, part of the subfamily Atelinae, family Atelidae. Like other atelines, they are found in tropical forests of Central and South America, from southern Mexico to Brazil. The genus consistes of seven species, all of which are under threat; the brown spider monkey is critically endangered. They are also notable for their ability to be easily bred in captivity.',
         image: './spider-monkey.jpg',
         exhibit_Id: 3
-
       }
     ])
   },
 
   async down(queryInterface, Sequelize) {
     return queryInterface.bulkDelete('animals', null, {})
-
   }
 }
